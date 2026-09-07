181aef78068871da3cb32a4d1d21015cf3d61cb8
Scott Sutton <scosut@hotmail.com>
2026-09-07 12:04:22 -0700

    Prepare for deployment.

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ const port = process.env.PORT || 8000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/groups', groupRouter);
